876806c21218cebc3e4d140f088ee2d7c35cbdfd
PavelBrui <117356628+pavelbrui@users.noreply.github.com>
2023-08-01 11:41:57 +0200

    Fix and update gei crud filter parameters (#4)
    * fixErrorAndAddObjectFilter
    
    * addRegexFilter
    
    * schemaUp

--- packages/integrations/gei-crud/src/Query/objects.ts
+++ packages/integrations/gei-crud/src/Query/objects.ts
@@ -6,7 +6,25 @@ export const handler = async (input: FieldResolveInput) => {
   return DB().then((db) => {
     const filterInput = {
       ...prepareSourceParameters(input),
+      ...(input.arguments?.fieldFilter as object),
+      ...convertObjectToRegexFormat(input.arguments?.fieldFilterReg as QueryObject),
     };
+    console.log(filterInput);
+
     return db.collection(prepareModel(input)).find(filterInput).toArray();
   });
 };
+
+interface QueryObject {
+  [key: string]: unknown;
+}
+
+// Function to convert the object to the desired format
+function convertObjectToRegexFormat(obj: QueryObject): QueryObject | undefined {
+  const obj2: QueryObject = {};
+
+  for (const key in obj) {
+    if (obj[key]) obj2[key] = { $regex: obj[key], $options: 'i' };
+  }
+  return obj2 || {};
+}
