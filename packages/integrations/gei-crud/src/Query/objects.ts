858a1044ecfc9909591555c52e40a5811d89bc9d
Pavel Brui <pavelbrui@aexol.com>
2023-08-01 14:25:40 +0200

    addSort

--- packages/integrations/gei-crud/src/Query/objects.ts
+++ packages/integrations/gei-crud/src/Query/objects.ts
@@ -9,14 +9,17 @@ export const handler = async (input: FieldResolveInput) => {
       ...(input.arguments?.fieldFilter as object),
       ...convertObjectToRegexFormat(input.arguments?.fieldFilterReg as QueryObject),
     };
+   const sort: {field: string, order: boolean} = input.arguments?.sortByField
+    
   
-    return db.collection(prepareModel(input)).find(filterInput).toArray();
+    return db.collection(prepareModel(input)).find(filterInput).sort(sort?.field ? { [snakeCaseToCamelCase(sort.field)]: sort.order === false ? -1 : 1 } : { _id: 1 }).toArray();
   });
 };
 
 interface QueryObject {
   [key: string]: unknown;
 }
+
 
 // Function to convert the object to the desired format
 function convertObjectToRegexFormat(obj: QueryObject): QueryObject | undefined {
@@ -26,4 +29,9 @@ function convertObjectToRegexFormat(obj: QueryObject): QueryObject | undefined {
     if (obj[key]) obj2[key] = { $regex: obj[key], $options: 'i' };
   }
   return obj2 || {};
+}
+
+
+function snakeCaseToCamelCase(input: string | null | undefined) {
+  return input?.toLowerCase().replace(/_([a-z])/g, (match, group1) => group1.toUpperCase());
 }
