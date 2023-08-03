7bf4eb5d2ff1ba3a0e3229c743443188df976e43
Pavel Brui <pavelbrui@aexol.com>
2023-08-03 15:58:22 +0200

    updateRelationInCrud

--- packages/integrations/gei-crud/src/Object/oneToMany.ts
+++ packages/integrations/gei-crud/src/Object/oneToMany.ts
@@ -3,17 +3,20 @@ import { prepareRelatedField, prepareRelatedModel } from '../data.js';
 import { DB } from '../db/mongo.js';
 
 export const handler = async (input: FieldResolveInput) =>
-  DB().then((db) => {
+  DB().then(async (db) => {
     const source = input.source;
     if (!source) {
       throw new Error(
         'Invalid input. This resolver work only if it is piped from other resolver. Either make it correct way or remove sourceParameters from resolver',
       );
     }
+  
     const s = source as Record<string, any>;
-    const field = prepareRelatedField(input);
+    const prepareField = prepareRelatedField(input).replace(/[{ }]/g, '').split(":");
+    const fieldForFounding = prepareField[0];
+    const fieldWithArray = prepareField[1] ? prepareField[1]  : undefined
+  
     return db
       .collection(prepareRelatedModel(input))
-      .find({ [field]: s._id })
-      .toArray();
+      .find( { [fieldForFounding]: (fieldWithArray? { $in: s[fieldWithArray]} :  s._id )}).toArray()
   });
