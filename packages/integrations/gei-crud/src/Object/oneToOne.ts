87a4e25246ea671d83b6360b8a57340f0224599e
PavelBrui <117356628+pavelbrui@users.noreply.github.com>
2023-08-03 15:59:34 +0200

    updateRelationInCrud (#7)

--- packages/integrations/gei-crud/src/Object/oneToOne.ts
+++ packages/integrations/gei-crud/src/Object/oneToOne.ts
@@ -11,6 +11,8 @@ export const handler = async (input: FieldResolveInput) =>
       );
     }
     const s = source as Record<string, any>;
-    const field = prepareRelatedField(input);
-    return db.collection(prepareRelatedModel(input)).findOne({ [field]: s._id });
+    const prepareField = prepareRelatedField(input).replace(/[{ }]/g, '').split(":");
+    const field = prepareField[0] ;
+    const arIds = prepareField[1] ?  prepareField[1] : undefined
+    return db.collection(prepareRelatedModel(input)).findOne({ [field]: arIds || s._id });
   });
