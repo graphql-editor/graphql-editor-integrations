6fc31ee3718533c99704561e4934b994ff938f2d
Matnabru <mateusz.urbanek@aexol.com>
2023-08-17 11:19:44 +0200

    gei-crud oneToMany fix

--- packages/integrations/gei-crud/src/Object/oneToMany.ts
+++ packages/integrations/gei-crud/src/Object/oneToMany.ts
@@ -2,21 +2,32 @@ import { FieldResolveInput } from 'stucco-js';
 import { prepareRelatedField, prepareRelatedModel } from '../data.js';
 import { DB } from '../db/mongo.js';
 
-export const handler = async (input: FieldResolveInput) =>
-  DB().then(async (db) => {
+export const handler = async (input: FieldResolveInput) => {
+  return DB().then(async (db) => {
     const source = input.source;
     if (!source) {
       throw new Error(
-        'Invalid input. This resolver work only if it is piped from other resolver. Either make it correct way or remove sourceParameters from resolver',
+        'Invalid input. This resolver works only if it is piped from another resolver. Either set it up correctly or remove sourceParameters from resolver',
       );
     }
-  
+
     const s = source as Record<string, any>;
-    const prepareField = prepareRelatedField(input).replace(/[{ }]/g, '').split(":");
+    const prepareField = prepareRelatedField(input).replace(/[{ }]/g, '').split(':');
     const fieldForFounding = prepareField[0];
-    const fieldWithArray = prepareField[1] ? prepareField[1]  : undefined
-    if(fieldWithArray&&!s[fieldWithArray]?.length) return null
+    const fieldWithArray = prepareField[1] ? prepareField[1] : undefined;
+
+    // Check if s[fieldWithArray] is indeed an array and is not empty.
+    if (fieldWithArray) {
+      if (!Array.isArray(s[fieldWithArray]) || s[fieldWithArray].length === 0) {
+        return null;
+      }
+    }
+
     return db
       .collection(prepareRelatedModel(input))
-      .find( { [fieldForFounding]: (fieldWithArray? { $in: s[fieldWithArray]} :  s._id )}).toArray()
+      .find({
+        [fieldForFounding]: fieldWithArray ? { $in: s[fieldWithArray] } : s._id,
+      })
+      .toArray();
   });
+};
