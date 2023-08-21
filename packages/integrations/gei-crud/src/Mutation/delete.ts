60e3c328b432c4216f2341b5835964aa824d6c21
Matnabru <mateusz.urbanek@aexol.com>
2023-08-21 16:34:40 +0200

    refactored delete for gei-crud

--- packages/integrations/gei-crud/src/Mutation/delete.ts
+++ packages/integrations/gei-crud/src/Mutation/delete.ts
@@ -1,44 +1,45 @@
 import { FieldResolveInput } from 'stucco-js';
-import { prepareModel, prepare_id, prepareSourceParameters, prepareRelatedField, prepareRelatedModel } from '../data.js';
+import {
+  prepareModel,
+  prepare_id,
+  prepareSourceParameters,
+  prepareRelatedField,
+  prepareRelatedModel,
+} from '../data.js';
 import { DB } from '../db/mongo.js';
 
-export const handler = async (input: FieldResolveInput) =>
-  DB().then(async (db) => {
-    const _id = prepare_id(input) || prepareSourceParameters(input)._id;
-    if (!_id) throw new Error('_id not found');
+export const handler = async (input: FieldResolveInput) => {
+  const db = await DB();
 
-    const object = await db
-    .collection(prepareModel(input))
-    .findOne({ _id })
-    if (!object) {
-      throw new Error(
-        'Object with this _id not found',
-      );
-    }
-    const res = await db
-      .collection(prepareModel(input))
-      .deleteOne({ _id, ...prepareSourceParameters(input) })
-    if(!res.deletedCount) throw new Error(`Object not found. Please check parameters: ${JSON.stringify({ _id, ...prepareSourceParameters(input) })}`)
+  const _id = prepare_id(input) || prepareSourceParameters(input)._id;
+  if (!_id) throw new Error('_id not found');
 
-    const s = object as Record<string, any>;
-    const prepareField = prepareRelatedField(input)?.replace(/[{ }]/g, '')?.split(":");
-    if(prepareField ){
+  const object = await db.collection(prepareModel(input)).findOne({ _id });
+  if (!object) {
+    throw new Error('Object with this _id not found');
+  }
+
+  const res = await db.collection(prepareModel(input)).deleteOne({ _id, ...prepareSourceParameters(input) });
+  if (!res.deletedCount) {
+    throw new Error(
+      `Object not found. Please check parameters: ${JSON.stringify({ _id, ...prepareSourceParameters(input) })}`,
+    );
+  }
+
+  const s = object as Record<string, any>;
+  const prepareField = prepareRelatedField(input)?.replace(/[{ }]/g, '').split(':');
+  if (prepareField) {
     const fieldForFounding = prepareField[0];
-    
-    const fieldWithArray = prepareField[1] ? prepareField[1]  : undefined
+    const fieldWithArray = prepareField[1] || undefined;
 
-    if(!fieldWithArray){
-       db.collection(prepareRelatedModel(input)).updateMany(
-        {},
-        { $pull: { [fieldForFounding]:  s._id  } }
-      );
-      return !!res.deletedCount
+    if (!fieldWithArray) {
+      await db.collection(prepareRelatedModel(input)).updateMany({}, { $pull: { [fieldForFounding]: s._id } });
+    } else if (!s[fieldWithArray]?.length) {
+      return !!res.deletedCount;
+    } else {
+      await db.collection(prepareRelatedModel(input)).deleteMany({ [fieldForFounding]: { $in: s[fieldWithArray] } });
     }
-    if(fieldWithArray&&!s[fieldWithArray]?.length) !!res.deletedCount
+  }
 
-     db.collection(prepareRelatedModel(input))
-      .deleteMany( { [fieldForFounding]: { $in: s[fieldWithArray]}
-        })
-      }
-    return !!res.deletedCount
-  });
+  return !!res.deletedCount;
+};
