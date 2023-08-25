ecd0d20da6ad8a61a4a5cf9601159c310ceda402
Pavel Brui <pavelbrui@aexol.com>
2023-08-25 17:59:20 +0200

    litleFixForCrudDelete

--- packages/integrations/gei-crud/src/Mutation/delete.ts
+++ packages/integrations/gei-crud/src/Mutation/delete.ts
@@ -10,7 +10,6 @@ import { DB } from '../db/mongo.js';
 
 export const handler = async (input: FieldResolveInput) => {
   const db = await DB();
-  
 
   const _id = prepare_id(input) || prepareSourceParameters(input)._id;
   if (!_id) throw new Error('_id not found');
@@ -29,28 +28,30 @@ export const handler = async (input: FieldResolveInput) => {
 
   const s = object as Record<string, any>;
 
-  const relatedCollections = prepareRelatedModel(input).replace(/["' ]/g, '').split(',')
+  const relatedCollections = prepareRelatedModel(input).replace(/["' ]/g, '').split(',');
   const prepareFields = prepareRelatedField(input)?.replace(/[{ }]/g, '').split(',');
-  let i = 0
-  for (const rC of relatedCollections){
-   const prepareField = prepareFields[i]?.split(':') || prepareFields[0]?.split(':');
-   i++
-   if (prepareField) {
-     const fieldForFounding = prepareField[0];
-     const fieldWithIdOrArray = prepareField[1] || undefined;
+  let i = 0;
+  for (const rC of relatedCollections) {
+    const prepareField = prepareFields[i]?.split(':') || prepareFields[0]?.split(':');
+    i++;
+    if (prepareField) {
+      const fieldForFounding = prepareField[0];
+      const fieldWithIdOrArray = prepareField[1] || undefined;
 
-     if (!fieldWithIdOrArray) {
-       await db.collection(rC).deleteMany({ [fieldForFounding]: s._id });
-       await db.collection(rC).updateMany({}, { $pull: { [fieldForFounding]: s._id } });
-     } else if (!s[fieldWithIdOrArray]?.length) {
-       return !!res.deletedCount;
-     } else {
-      if(typeof(s[fieldWithIdOrArray]) === "string"){
-        await db.collection(rC).deleteMany({ [fieldForFounding]:  s[fieldWithIdOrArray]  })
-      }else
-       {await db.collection(rC).deleteMany({ [fieldForFounding]: { $in: s[fieldWithIdOrArray] } })};
-     }
-   }
-}
+      if (!fieldWithIdOrArray) {
+        await db.collection(rC).updateMany({}, { $pull: { [fieldForFounding]: s._id } });
+        await db.collection(rC).deleteMany({ [fieldForFounding]: s._id });
+      } else if (!s[fieldWithIdOrArray]?.length) {
+        return !!res.deletedCount;
+      } else {
+        if (typeof s[fieldWithIdOrArray] === 'string') {
+          await db.collection(rC).updateMany({}, { $pull: { [fieldForFounding]: s[fieldWithIdOrArray] } });
+          await db.collection(rC).deleteMany({ [fieldForFounding]: s[fieldWithIdOrArray] });
+        } else {
+          await db.collection(rC).deleteMany({ [fieldForFounding]: { $in: s[fieldWithIdOrArray] } });
+        }
+      }
+    }
+  }
   return !!res.deletedCount;
 };
