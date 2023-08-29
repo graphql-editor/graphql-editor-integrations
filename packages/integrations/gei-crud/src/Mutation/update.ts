3df883148a705ec569a5f74293ca1cfb9b1c56d8
Pavel Brui <pavelbrui@aexol.com>
2023-08-29 15:18:19 +0200

    updateGeiCrudUpdate

--- packages/integrations/gei-crud/src/Mutation/update.ts
+++ packages/integrations/gei-crud/src/Mutation/update.ts
@@ -7,19 +7,28 @@ export const handler = async (input: FieldResolveInput) =>
     const _id = prepare_id(input) || (prepareSourceParameters(input)._id as string);
     if (!_id) throw new Error('_id not found');
     const entries = Object.entries(input.arguments || {});
-    if (entries.length !== 2 && entries.length !== 1) {
-      throw new Error(
-        'There should be only 2 arguments one "String" _id argument and one of "input" type to update this model',
-      );
-    }
-    const setter = entries.find((e) => e[0] !== '_id');
-    if (!setter) {
+    const reconstructedObject: Record<string, any> = {};
+    
+    const entriesWithOutId = entries.filter((e) => e[0] !== '_id');
+    if (!entriesWithOutId) {
       throw new Error(`You need update input argument for this resolver to work`);
     }
+    
+    if (typeof entriesWithOutId[0][1] ==='object' ) {
+      if(entriesWithOutId[1]) throw new Error(
+        'There should be only string arguments or _id argument and one argument of "input" type to update this model',
+      )
+    } else{
+      (entriesWithOutId  as [string, any][]).forEach((entry: [string, any]) => {
+        const [key, value] = entry;
+        reconstructedObject[key] = value;
+      });
+    }
+    const setter = typeof entriesWithOutId[0][1] ==='object' ? entriesWithOutId[0][1] :  reconstructedObject
     const filterInput: Record<string, any> = { _id, ...prepareSourceParameters(input) };
     const res = await db
     .collection(prepareModel(input))
-    .updateOne(filterInput, { $set: { ...(setter[1] as object), updatedAt: new Date().toISOString() } })
+    .updateOne(filterInput, { $set: { ...(setter), updatedAt: new Date().toISOString() } })
     if(res.matchedCount<1) throw new Error(`Object for update not found. Please check parameters: ${ JSON.stringify(filterInput)}`)
     return res.modifiedCount>=1
   });
