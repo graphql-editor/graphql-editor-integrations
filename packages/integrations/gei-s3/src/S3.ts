2d9fd54b728ecfd7d5c492b3ae2dac7fdd25c3e7
Matnabru <mateusz.urbanek@aexol.com>
2023-09-12 15:50:35 +0200

    gei-s3 aws-sdk update

--- packages/integrations/gei-s3/src/S3.ts
+++ packages/integrations/gei-s3/src/S3.ts
@@ -1,11 +1,12 @@
-import AWS from 'aws-sdk';
+import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-interface S3Client {
-  client: AWS.S3;
+interface S3ClientDetails {
+  client: S3Client;
   bucket: string;
 }
 
-let s3: S3Client | undefined;
+let s3: S3ClientDetails | undefined;
 
 export function getEnv<T extends string>(val: T[]): Record<T, string> {
   const ret: Partial<Record<T, string>> = {};
@@ -19,7 +20,7 @@ export function getEnv<T extends string>(val: T[]): Record<T, string> {
   return ret as Record<T, string>;
 }
 
-const S3Client = () => {
+const initializeS3Client = (): S3ClientDetails => {
   if (s3) return s3;
 
   const { SPACES_BUCKET, SPACES_REGION, SPACES_KEY, SPACES_SECRET } = getEnv([
@@ -29,17 +30,18 @@ const S3Client = () => {
     'SPACES_SECRET',
   ]);
 
-  let spacesEndpoint;
-  const endpoint = process.env.SPACES_ENDPOINT;
-  if (endpoint) spacesEndpoint = new AWS.Endpoint(`${SPACES_REGION}.${endpoint}`);
+  const endpoint = process.env.SPACES_ENDPOINT ? `${SPACES_REGION}.${process.env.SPACES_ENDPOINT}` : undefined;
 
   s3 = {
-    client: new AWS.S3({
-      endpoint: spacesEndpoint,
-      accessKeyId: SPACES_KEY,
-      secretAccessKey: SPACES_SECRET,
+    client: new S3Client({
+      region: SPACES_REGION,
+      credentials: {
+        accessKeyId: SPACES_KEY,
+        secretAccessKey: SPACES_SECRET
+      },
+      endpoint: endpoint
     }),
-    bucket: SPACES_BUCKET,
+    bucket: SPACES_BUCKET
   };
 
   return s3;
@@ -52,16 +54,15 @@ export const putUrl = async ({
   fileKey: string;
   contentType: string;
 }): Promise<{ fileKey: string; putUrl: string }> => {
-  const s3 = S3Client();
-  const params = {
+  const s3 = initializeS3Client();
+  const command = new PutObjectCommand({
     Bucket: s3.bucket,
     Key: fileKey,
     ContentType: contentType,
     ACL: 'public-read',
-  };
-  const putUrl = s3.client.getSignedUrl('putObject', {
-    ...params,
   });
+  
+  const putUrl = await getSignedUrl(s3.client, command);
 
   return {
     fileKey,
@@ -73,14 +74,13 @@ export const getUrl = async (fileKey: string | undefined): Promise<string> => {
   if (!fileKey || fileKey === '') return '';
   if (fileKey.includes('https')) return fileKey;
 
-  const s3 = S3Client();
-  const params = {
+  const s3 = initializeS3Client();
+  const command = new GetObjectCommand({
     Bucket: s3.bucket,
-    Key: fileKey,
-  };
-  const getUrl = s3.client.getSignedUrl('getObject', {
-    ...params,
+    Key: fileKey
   });
+  
+  const getUrl = await getSignedUrl(s3.client, command);
 
   return getUrl;
 };
