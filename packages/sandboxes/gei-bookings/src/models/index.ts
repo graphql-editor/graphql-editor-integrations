16b250fbb8e21ae981ba912261fbfffe321438d7
TakSeBiegam <kurylo.ao@gmail.com>
2023-09-13 12:00:02 +0200

    feat: generated bookings-sandbox resolvers

---
+++ packages/sandboxes/gei-bookings/src/models/index.ts
@@ -0,0 +1,14 @@
+import { UserQueryModel } from './UserQueryModel.js'
+import { PublicQueryModel } from './PublicQueryModel.js'
+import { UserMutationModel } from './UserMutationModel.js'
+import { MutationModel } from './MutationModel.js'
+import { QueryModel } from './QueryModel.js'
+
+
+export type Models = {
+	UserQueryModel: UserQueryModel;
+	PublicQueryModel: PublicQueryModel;
+	UserMutationModel: UserMutationModel;
+	MutationModel: MutationModel;
+	QueryModel: QueryModel;
+};
