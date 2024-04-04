545ae3f1b082f766af97cf0e991b81815706cdc4
Pavel Brui <pavelbrui@aexol.com>
2024-04-04 10:20:14 +0200

    updateBook

--- packages/integrations/gei-bookings/src/UserMutation/bookService.ts
+++ packages/integrations/gei-bookings/src/UserMutation/bookService.ts
@@ -2,7 +2,8 @@ import { FieldResolveInput } from 'stucco-js';
 import { BookStatus, resolverFor } from '../zeus/index.js';
 import { orm } from '../utils/db/orm.js';
 import { GlobalError, errMiddleware, sourceContainUserIdOrThrow } from '../utils/middleware.js';
-import { ObjectId } from 'mongodb';
+import { ObjectId, WithId } from 'mongodb';
+import { ServiceModel } from '../models/ServiceModel.js';
 
 export const bookService = async (input: FieldResolveInput) =>
   resolverFor('UserMutation', 'bookService', async (args, src) =>
@@ -10,19 +11,34 @@ export const bookService = async (input: FieldResolveInput) =>
       sourceContainUserIdOrThrow(src);
       const o = await orm();
 
-      const services = await Promise.all(args.input.serviceIds.map(async (serviceId) => {
-
-       const service = await o('Services').collection.findOneAndUpdate(
-        { _id: serviceId, taken: { $ne: true } },
-        { $set: { taken: true } },
-      );
-      if (!service.value) {
-        throw new GlobalError(`service is already taken: ${serviceId}`, import.meta.url);
-      }
-     
-      return service.value 
-    }))
     
+    const bookServices = await Promise.all(
+      args.input.serviceIds.map(
+        async (serviceId: string) =>
+          (
+            await o('Services').collection.findOneAndUpdate(
+              { _id: serviceId, taken: { $ne: true } },
+              { $set: { taken: true } },
+            )
+          ).value || serviceId,
+      ),
+    );
+    const [bookedServices, busy] = [
+      bookServices.filter((s): s is WithId<ServiceModel>  => typeof s !== 'string'),
+      bookServices.filter((s): s is string => typeof s === 'string')
+  ];
+    bookServices.forEach(async (s) => {
+      if (typeof s === 'string') {
+        await o('Services').collection.updateMany(
+          { _id: { $in: bookedServices.map((s: any) => s._id) } },
+          { $set: { taken: false } },
+        );
+        throw new GlobalError(`Service is already taken: ${s}`, import.meta.url);
+      }
+      return s._id;
+    });
+
+
       const book = await o('Bookings')
         .collection.insertOne(
           {
@@ -31,13 +47,13 @@ export const bookService = async (input: FieldResolveInput) =>
           bookerId: src.userId,
           services: args.input.serviceIds,
           comments: args.input.comments ? args.input.comments : undefined,
-          status: services[0].neededAccept ? BookStatus.PENDING : BookStatus.ACCEPTED,
+          status: bookedServices[0].neededAccept ? BookStatus.PENDING : BookStatus.ACCEPTED,
         })
         .then(async (c) => o('Bookings').collection.findOne({ _id: c.insertedId } ));
       if (!book) {
         throw new GlobalError('inserted document is null', import.meta.url);
       }
-      return { book: { ...book, services: services }};
+      return { book: { ...book, services: bookedServices }};
     }),
   )(input.arguments, input.source);
 export default bookService;
